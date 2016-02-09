457ed29297e509cd64698abcbd86c6fa8a2cda8f
Pafoua <psfang@uci.edu>
2016-02-08 17:34:17 -0800

    Jump through platforms and a simple jump script.

---
+++ Jasper/Assets/Scripts/SimpleJump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleJump : MonoBehaviour {
+    public float jumpspeed = 300f;
+    public Vector2 jumpVector;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+    // Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Space))
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpspeed));
+    }
+     
+	}
+}
