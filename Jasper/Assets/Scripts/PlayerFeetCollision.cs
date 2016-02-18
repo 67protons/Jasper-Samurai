e741df64754585f15734a6a1ca60bc0670290347
eaho1 <eaho1@uci.edu>
2016-02-17 17:35:39 -0800

    Finished smashable blocks =D

--- Jasper/Assets/Scripts/PlayerFeetCollision.cs
+++ Jasper/Assets/Scripts/PlayerFeetCollision.cs
@@ -5,19 +5,44 @@ public class PlayerFeetCollision : MonoBehaviour {
     [HideInInspector]
     public bool isGrounded = false;
 
-    void OnTriggerEnter2D(Collider2D hitObject)
+    private Player player;
+
+    void Awake()
     {
+        player = this.transform.parent.GetComponent<Player>();
+    }
+
+    void OnTriggerStay2D(Collider2D hitObject)
+    {        
         if (hitObject.CompareTag("Terrain"))
         {
             isGrounded = true;
         }
-    }
+        if (hitObject.CompareTag("Breakable"))
+        {           
+            if (player.smashing)
+            {
+                Collider2D[] hits = Physics2D.OverlapCircleAll((Vector2)this.transform.position + this.GetComponent<CircleCollider2D>().offset, .4f);
+                foreach (Collider2D hit in hits)
+                {
+                    if (hit.CompareTag("Breakable"))
+                    {
+                        Destroy(hit.transform.parent.parent.gameObject);
+                    }
+                }                
+            }
+            else
+            {
+                isGrounded = true;
+            }
+        }
+    }    
 
     void OnTriggerExit2D(Collider2D hitObject)
     {
-        if (hitObject.CompareTag("Terrain"))
+        if (hitObject.CompareTag("Terrain") || hitObject.CompareTag("Breakable"))
         {
             isGrounded = false;
-        }
+        }        
     }
 }
