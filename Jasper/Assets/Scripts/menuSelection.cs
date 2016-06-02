24cd562c22f515d65f74aa8359d489885762fb46
eaho1 <eaho1@uci.edu>
2016-06-01 17:28:54 -0700

    cutscene done

--- Jasper/Assets/Scripts/menuSelection.cs
+++ Jasper/Assets/Scripts/menuSelection.cs
@@ -1,51 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class menuSelection : MonoBehaviour 
 {
-    public GameObject startMenu;
-    public GameObject mainMenu;
-
-    private bool onStartMenu = true;
-
-
-    void Start()
-    {
-        mainMenu.SetActive(true);
-        //getStartMenu();
-    }
-
-    void Update()
-    {
-        //if(Input.GetKeyDown("return") && onStartMenu == true)
-        //{
-        //    pressStart();
-           
-        //}
-    }
-
-    //public void pressStart()
-    //{
-    //    startMenu.SetActive(false);
-    //    mainMenu.SetActive(true);
-    //    onStartMenu = false;
-    //}
-
-    //public void getStartMenu()
-    //{
-    //    mainMenu.SetActive(false);
-    //    startMenu.SetActive(true);
-    //    onStartMenu = true;
-    //}
+    public GameObject SelectionPanel, CutscenePanel;
+    public Sprite[] cutscenes;
+    public AudioSource mainTrack;
 
     public void startGame()
-    {        
-        SceneManager.LoadScene(1);
+    {
+        SelectionPanel.SetActive(false);
+        PlayCinematic();
+        //SceneManager.LoadScene(1);
+        StartCoroutine(EndCutscene());
     }
 
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void PlayCinematic()
+    {
+        Image panelImage = CutscenePanel.GetComponent<Image>();
+        Animator panelAnimator = CutscenePanel.GetComponent<Animator>();
+        panelAnimator.Play("fadeIn");
+
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    cutscene.sprite = cutscenes[i];
+        //}
+    }
+
+    private IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds(18f);
+        float startTime = Time.time;
+        while (Time.time < startTime + 2f)
+        {
+            mainTrack.volume = (2 - (Time.time - startTime));
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene(1);
     }
 }
